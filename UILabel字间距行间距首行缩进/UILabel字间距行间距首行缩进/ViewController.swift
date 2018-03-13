1772fdee9b64d9e94ab6e4eec5c70e4c0b1ec72a
mac <JOE@macdemac-mini.local>
2018-03-13 16:14:14 +0800

    - commit

--- UILabel字间距行间距首行缩进/UILabel字间距行间距首行缩进/ViewController.swift
+++ UILabel字间距行间距首行缩进/UILabel字间距行间距首行缩进/ViewController.swift
@@ -29,15 +29,15 @@ extension ViewController {
     
     fileprivate func setPage() {
         
-        let labelText = "字间距行间距首行缩进字间距行间距   首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进 \n 字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进字间距行间距首行缩进"
+        let labelText = "字间距行间距首行缩进字间距行间距 "
         let labelText1 = "字间距行间距首行缩进字间距行间距"
         
         let label = UILabel(text: labelText, font: UIFont.systemFont(ofSize: 17), textColor: UIColor.white, textAlignment: .left, numberOfLines: 0)
         label.backgroundColor = UIColor.orange
-        label.ZZJ_setLineSpace(withLineSpace: 5, withLabelText: labelText, withFont: UIFont.systemFont(ofSize: 17), withZSpace: 5)
+        label.ZZJ_setLineSpace(withLineSpace: 12, withLabelText: labelText, withFont: UIFont.boldSystemFont(ofSize: 17), withZSpace: 5)
         
-//        let labelH = label.ZZJ_getSpaceLabelHeight(withText: labelText, withFont: UIFont.systemFont(ofSize: 17), withWidth: view.bounds.size.width, withLineSpace: 5, withZSpace: 5)
-        let labelH = label.ZZJ_getIndentationLabelHeight(withText: labelText, withStrIndentationNum: 5, withNSTextAlignment: .left, withFontSize: 17, withWidth: view.bounds.size.width, withLineSpace: 5, withZSpace: 5)
+        let labelH = label.ZZJ_getSpaceLabelHeight(withText: labelText, withFont: UIFont.boldSystemFont(ofSize: 17), withWidth: view.bounds.size.width, withLineSpace: 12, withZSpace: 5)
+//        let labelH = label.ZZJ_getIndentationLabelHeight(withText: labelText, withStrIndentationNum: 5, withNSTextAlignment: .left, withFontSize: 17, withWidth: view.bounds.size.width, withLineSpace: 12, withZSpace: 5)
         label.frame = CGRect(x: 0, y: 100, width: view.bounds.size.width, height: labelH)
         
         view.addSubview(label)
